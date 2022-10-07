6f48e7ee486b7e0c27d14e8f126058cc34e7ddd7
andylaurito <andres.laurito@providers.glovoapp.com>
2022-10-07 17:36:45 +0200

    testing out stuff

--- braveclojure/clojure-noob/src/clojure_noob/core.clj
+++ braveclojure/clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a teapot! :)"))
+
+(defn my-print 
+  "Testing in runtime"
+  [something]
+  (println something))
+
+(defn train
+  "Testing in runtime"
+  []
+  (println "New method!"))
